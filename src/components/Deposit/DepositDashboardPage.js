f44fe1da726cb717675331a258ddf8d8e9a9627a
github username <github email address>
2018-06-02 19:11:23 -0500

    Add Filtering to Deposits Dashboard

--- src/components/Deposit/DepositDashboardPage.js
+++ src/components/Deposit/DepositDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import DepositSummary from './DepositSummary'
 import DepositList from './DepositList'
+import DepositListFilters from './DepositListFilters'
 
 const DepositDashboardPage = () => (
   <div>
     <DepositSummary />
+    <DepositListFilters />
     <DepositList />
   </div>
 )
